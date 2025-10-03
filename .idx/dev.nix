690b36c9e870aa0dc39d4b724c0c018d9494579e
reynish <reynish@gmail.com>
2025-10-03 12:04:46 +0000

    updated config

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,12 +6,8 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    # pkgs.go
-    # pkgs.python311
-    # pkgs.python311Packages.pip
     pkgs.nodejs_20
     pkgs.firebase-tools
-    # pkgs.nodePackages.nodemon
   ];
 
   # Sets environment variables in the workspace
